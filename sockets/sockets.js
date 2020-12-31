e063a3825d3aea617f8d6f42ba0c34c6b4f71aef
Jonathan Loboguerrero <joboguerrero@gmail.com>
2020-12-31 09:24:27 -0500

    Fin seccion 3

---
+++ sockets/sockets.js
@@ -0,0 +1,19 @@
+// Mensajes de sockets
+
+const { io } = require('../index');
+
+io.on('connection', client => {
+
+    console.log('Cliente conectado');
+
+    client.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+
+    client.on('mensaje', ( payload ) => {
+        console.log('Mensaje!!', payload);
+
+        io.emit( 'mensaje', { admin: 'Nuevo mensaje' } );
+    });
+
+  });
